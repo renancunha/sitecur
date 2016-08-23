f038bf0f49eef7981461069bac1009d91967d30c
Renan Cunha <renan.bara@hotmail.com>
2016-08-23 11:31:24 -0300

    alterado broadcast de dados

--- server/api/sensor_data/sensor_data.controller.js
+++ server/api/sensor_data/sensor_data.controller.js
@@ -98,8 +98,8 @@ export function receive(req, res) {
               sensorData.date = nowDate;
               sensorData.value = query[q];
               sensorData.sensor = sensor._id;
-
-            if(sDatas.length == 0 || (nowDate.diff(moment(sDatas[0].date), 'minutes')) >= 10) {
+              console.log((nowDate.diff(moment(sDatas[0].date), 'minutes'));
+            if(sDatas.length == 0 || (nowDate.diff(moment(sDatas[0].date), 'minutes') >= 10)) {
               console.log('Ja se passaram 10 minutos, insere no banco de dados...');
               sensorData.save()
                 .then(s => {
